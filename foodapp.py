548dc1aab700f7c81b91b164b3fad432653baa28
Payal <payal92310@gmail.com>
2018-05-06 15:28:15 +0530

    Added the functionality of the Cal button

--- foodapp.py
+++ foodapp.py
@@ -12,7 +12,19 @@ root.geometry("300x300")
 root.configure(bg="gray")
 
 v=StringVar()
-x=StringVar()
+amount=IntVar()
+result=IntVar()
+
+
+def print_result():
+    val = int(v.get())
+    if val==0:
+        result.set(int(entryAmount.get())* 4)   # Assuming Rs. 4 for an apple        
+    elif val==1:
+        result.set(int(entryAmount.get())* 5)   # Assuming Rs. 5 for an Mango        
+    elif val==2:
+        result.set(int(entryAmount.get())* 8)   # Assuming Rs. 8 for an Fanas        Fanas is Maharashtrian word for Jackfruit
+        
 label=Label(root, text="Choose item",width=20,bg="gray")
 label.place(x=90,y=10)
 
@@ -28,18 +40,17 @@ btn3.place(x=30,y=120)
 labelResult=Label(root, text="Result:",width=5,bg="gray",highlightbackground="gray")
 labelResult.place(x=30,y=150)
 
-entryResult=Entry(root,width=20)
+entryAmount=Entry(root,width=20)
+entryAmount.place(x=100,y=190)
+
+
+entryResult=Entry(root,width=20,textvariable=result)
 entryResult.place(x=100,y=150)
 
-btnCal=Button(root,text="Calc",width=5,highlightbackground="gray",bg="gray",height=0)
+btnCal=Button(root,text="Calc",width=5,highlightbackground="gray",bg="gray",height=0, command= lambda: print_result())
 btnCal.place(x=26,y=187)
 
-str=StringVar()
-str.set("heylo")
-entryAmount=Entry(root,width=20)
-print(dir(entryAmount))
-entryAmount.place(x=100,y=190)
-print(entryAmount.get())
+
 
 
 root.mainloop()
